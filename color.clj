24a6e81c12f6d7c3b48b763bfacc3b44a1602ee8
Danny <DeDanny@gmail.com>
2010-09-18 12:51:07 +0200

    Caclucation works. Made use of bar proofer to test the math. draw-bars is now next step

--- color.clj
+++ color.clj
@@ -37,20 +37,46 @@
     (alter main-bar x-update x)
        ))
 
-(defn draw-balk [g x color]
+(defn draw-bar [g x color]
   (.setColor g color)
   (.fillRect g x 0 bar-size height))
 
-(defn bar-calculator [g {x :location red :red green :green blue :blue}]
-    (printer (ceil (/ width bar-size)) width bar-size)
-    (draw-balk g x (Color. red green blue))
+;(defn bar-calculator
+;  "Calculator works from the outside in. so f must me inverted"
+;  [f m]
+;  (fn
+;    [bars x red green blue]
+;      (if (zero? bars)
+;        '()
+;        (cons (f (* bars x) bar-size) (- 255 (* bars red)) (- 255 (* bars green)) (- 255 (* bars blue)) (m (dec bars) x red green blue))
+;        )))
+
+(defn calculate-color [bar color]
+  (let [color-number (- 255 (* bar color))]
+    (if (< color-number 0)
+      0
+      color-number)))
+
+(defn calculate-right [bars x red green blue]
+      (if (zero? bars)
+        '()
+        (cons (list (+ x (* bars bar-size)) (calculate-color bars red) (calculate-color bars green) (calculate-color bars blue)) (calculate-right (dec bars) x red green blue))
+        ))
+
+
+(defn bar-draw [g {x :location red :red green :green blue :blue}]
+    ;(printer (Math/ceil (/ (- width (Math/floor (+ x bar-size))) bar-size )))
+    (calculate-right(Math/ceil (/ (- width (Math/floor (+ x bar-size))) bar-size )) x red green blue)
+    ;(bar-loop (Math/ceil (/ width bar-size)) x #(+ bar-size %) red green blue)
+    (draw-bars (calculate-right(Math/ceil (/ (- width (Math/floor (+ x bar-size))) bar-size )) x red green blue))
+    (draw-bar g x (Color. 255 255 255))
   )
 
 (defn color-panel [frame main-bar]
   (proxy [JPanel MouseListener MouseMotionListener ] []
     (paintComponent [g]
       (proxy-super paintComponent g)
-      (bar-calculator g @main-bar)
+      (bar-draw g @main-bar)
       )
     (mouseMoved [e]
       (set-zero-bar main-bar (.getX e))
